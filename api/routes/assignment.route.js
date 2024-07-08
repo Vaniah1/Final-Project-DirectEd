35f68ffb6d9f0c8bf59eb4b4013fa39ee6358cdc
vaniah1 <ndunguvaniah@gmail.com>
2024-07-08 17:50:57 +0300

    fixed some typos

--- api/routes/assignment.route.js
+++ api/routes/assignment.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createAssignment, deleteAssignment, getAllAssignments, getAssignmentById, updateAssignment } from '../controllers/assignment.controller'
+import { createAssignment, deleteAssignment, getAllAssignments, getAssignmentById, updateAssignment } from '../controllers/assignment.controller.js'
 
 const router = express.Router()
 

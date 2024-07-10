f2bc520101cb651318e277a543a9b58bf17d6ca6
Muri-tuu <kenhopkins001@gmail.com>
2024-07-10 14:20:38 +0300

    Imlementing routing and authentication work flow

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,46 +1,44 @@
-import React from 'react'
+import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { useSelector } from 'react-redux';
-
-
 
 const App = () => {
   const { currentRole } = useSelector(state => state.user);
 
   return (
     <Router>
-      {currentRole === null &&
+      {currentRole === null && 
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/choose" element={<ChooseUser visitor="normal" />} />
-          <Route path="/chooseasguest" element={<ChooseUser visitor="guest" />} />
-
+          <Route path="/chooseasguest" element={<chooseUser visitor="guest" />} />
+          
           <Route path="/Adminlogin" element={<LoginPage role="Admin" />} />
           <Route path="/Studentlogin" element={<LoginPage role="Student" />} />
-          <Route path="/Teacherlogin" element={<LoginPage role="Teacher" />} />
+          <Route path="/Teacherlogin" element={<LoginPage role="Teacher" />} /> 
 
-          <Route path="/Adminregister" element={<AdminRegisterPage />} />
-
+          <Route path="/AdminRegister" element={<AdminRegisterPage />} />
+          
           <Route path='*' element={<Navigate to="/" />} />
+          
         </Routes>}
-
+      
       {currentRole === "Admin" &&
         <>
           <AdminDashboard />
         </>
       }
-
       {currentRole === "Student" &&
         <>
-          <StudentDashboard />
+        <StudentDashboard />
+        </>
+      }
+      {currentRole === "Teacher" &&
+        <>
+        <TeacherDashboard />
         </>
       }
 
-      {currentRole === "Teacher" &&
-        <>
-          <TeacherDashboard />
-        </>
-      }
     </Router>
   )
 }

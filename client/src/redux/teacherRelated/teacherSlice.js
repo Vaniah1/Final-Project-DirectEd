7a9cfe32ceb604173a84a8ff889d62dccc9de786
Muri-tuu <kenhopkins001@gmail.com>
2024-07-10 17:34:08 +0300

    Implementing teacher slice with redux toolkit for managing teacher data state

--- client/src/redux/teacherRelated/teacherSlice.js
+++ client/src/redux/teacherRelated/teacherSlice.js
@@ -1,28 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    studentsList: [],
+    teachersList: [],
+    teacherDetails: [],
     loading: false,
     error: null,
     response: null,
-    statestatus: "idle",
 };
 
-const studentSlice = createSlice({
-    name: 'student',
+const teacherSlice = createSlice({
+    name: 'teacher',
     initialState,
     reducers: {
         getRequest: (state) => {
             state.loading = true;
         },
-        stuffDone: (state) => {
+        doneSuccess: (state, action) => {
+            state.teacherDetails = action.payload;
             state.loading = false;
             state.error = null;
             state.response = null;
-            state.statestatus = "added";
         },
         getSuccess: (state, action) => {
-            state.studentsList = action.payload;
+            state.teachersList = action.payload;
             state.loading = false;
             state.error = null;
             state.response = null;
@@ -36,24 +36,21 @@ const studentSlice = createSlice({
             state.loading = false;
             state.error = action.payload;
         },
-        underStudentControl: (state) => {
+        postDone: (state) => {
             state.loading = false;
-            state.response = null;
             state.error = null;
-            state.statestatus = "idle";
+            state.response = null;
         }
     },
 });
 
-
 export const {
     getRequest,
     getSuccess,
-     getFailed,
+    getFailed,
     getError,
-    underStudentControl,
-    stuffDone,
+    doneSuccess,
+    postDone
+} = teacherSlice.actions;
 
-}= studentSlice.actions;
-
-export const studentReducer = studentSlice.reducer;
+export const teacherReducer = teacherSlice.reducer;

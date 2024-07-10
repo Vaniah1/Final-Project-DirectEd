72e3899215e762ecc63fd427607881e54ff9b0a9
vaniah1 <ndunguvaniah@gmail.com>
2024-07-10 12:52:38 +0300

    App.jsx and tailwind custom color

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,47 @@
-
-import './App.css'
-
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { useSelector } from 'react-redux';
+
+
 
 const App = () => {
+  const { currentRole } = useSelector(state => state.user);
+
   return (
-    <div>
-      App
-    </div>
+    <Router>
+      {currentRole === null &&
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/choose" element={<ChooseUser visitor="normal" />} />
+          <Route path="/chooseasguest" element={<ChooseUser visitor="guest" />} />
+
+          <Route path="/Adminlogin" element={<LoginPage role="Admin" />} />
+          <Route path="/Studentlogin" element={<LoginPage role="Student" />} />
+          <Route path="/Teacherlogin" element={<LoginPage role="Teacher" />} />
+
+          <Route path="/Adminregister" element={<AdminRegisterPage />} />
+
+          <Route path='*' element={<Navigate to="/" />} />
+        </Routes>}
+
+      {currentRole === "Admin" &&
+        <>
+          <AdminDashboard />
+        </>
+      }
+
+      {currentRole === "Student" &&
+        <>
+          <StudentDashboard />
+        </>
+      }
+
+      {currentRole === "Teacher" &&
+        <>
+          <TeacherDashboard />
+        </>
+      }
+    </Router>
   )
 }
 

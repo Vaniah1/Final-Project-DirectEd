9042bfdd10c2f7a0c680b756e3e7b6c656968df0
vaniah1 <ndunguvaniah@gmail.com>
2024-07-12 13:54:03 +0300

    serve static site

--- backend/index.js
+++ backend/index.js
@@ -43,6 +43,17 @@ app.post('/verify-face', async (req, res) => {
 
     res.status(401).json({ message: 'Access Denied' });
 });
+const path = require('path')
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/dist')))
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"))
+})
+
 
 
 app.listen(PORT, () => {

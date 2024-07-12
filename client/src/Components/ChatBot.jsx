5e234aa531d6d37fe1a2ee7b9cbf909a1172ad87
vaniah1 <ndunguvaniah@gmail.com>
2024-07-12 11:02:24 +0300

    .

--- client/src/Components/ChatBot.jsx
+++ client/src/Components/ChatBot.jsx
@@ -70,7 +70,7 @@ const ChatWindow = styled.div`
   bottom: 80px;
   right: 0;
   width: 500px;
-  height: 600px;
+  height: 500px;
   background-color: white;
   border-radius: 12px;
   overflow: hidden;

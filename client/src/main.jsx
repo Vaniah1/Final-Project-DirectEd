4fac5d346d9fc93f501a41b5bd220c62910ba473
vaniah1 <ndunguvaniah@gmail.com>
2024-07-12 13:37:09 +0300

    removed theme provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,16 +4,14 @@ import './index.css';
 import App from './App';
 import store from './redux/store';
 import { Provider } from 'react-redux';
-import ThemeProvider from './components/ThemeProvider';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <ThemeProvider>
+    
       <App />
-    </ThemeProvider> 
     </Provider>
   </React.StrictMode>
 )

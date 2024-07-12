433d4b4a852c0a40dba8ef05f3804bf95a654598
vaniah1 <ndunguvaniah@gmail.com>
2024-07-12 14:43:55 +0300

    vite config adjust for build

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+  },
+  plugins: [react(),visualizer({ open: true }),],
   resolve: {
     alias: {
       '@mui/styled-engine': '@mui/styled-engine-sc'

5905d081d15093d745b5f73a6a39faa2703862f4
vaniah1 <ndunguvaniah@gmail.com>
2024-07-13 21:18:04 +0300

    .

--- backend/index.js
+++ backend/index.js
@@ -48,7 +48,7 @@ app.use('/', Routes);
 const path = require('path')
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client' ,'dist')))
+app.use(express.static(path.join(__dirname, '/client/dist')))
 
 // The "catchall" handler: for any request that doesn't
 // // match one above, send back React's index.html file.

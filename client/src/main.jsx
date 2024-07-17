980603609359110eb8bccce1159cd25b75128a8d
Muri-tuu <kenhopkins001@gmail.com>
2024-07-17 18:31:27 +0300

    Providing the redux store to react

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,10 +5,11 @@ import App from './App';
 import store from './redux/store';
 import { Provider } from 'react-redux';
 
-
+//Providing the redux store to react
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+     
     <Provider store={store}>
     
       <App />
